374943c77991f606c6634d918e663bf8f253b240
Carlos Reyes <carlosreyes8436@gmail.com>
2022-09-01 23:50:53 -0600

    initial

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+export {}
+
+const form = document.querySelector<HTMLFormElement>('.formulario')
+const space = document.querySelector<HTMLInputElement>('#tarea')
+const button = document.querySelector<HTMLButtonElement>('.boton')
+const list = document.querySelector<HTMLLIElement>('.listado_tareas')
+
+console.log(form)
